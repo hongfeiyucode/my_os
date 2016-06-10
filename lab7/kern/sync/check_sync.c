d6f4f8bbfdfc894f37a4fad17a06e075efdb58bb
hongfeiyucode <hongfeiyu@mail.cnu.edu.pl>
2016-06-11 02:19:11 +0800

    LAB 7 1~2

--- lab7/kern/sync/check_sync.c
+++ lab7/kern/sync/check_sync.c
@@ -123,7 +123,13 @@ void phi_take_forks_condvar(int i) {
 //--------into routine in monitor--------------
      // LAB7 EXERCISE1: YOUR CODE
      // I am hungry
-     // try to get fork
+     state_condvar[i]=HUNGRY; 
+      // try to get fork
+      phi_test_condvar(i); 
+      while (state_condvar[i] != EATING) {
+          cprintf("phi_take_forks_condvar: %d didn't get fork and will wait\n",i);
+          cond_wait(&mtp->cv[i]);
+      }
 //--------leave routine in monitor--------------
       if(mtp->next_count>0)
          up(&(mtp->next));
@@ -136,8 +142,11 @@ void phi_put_forks_condvar(int i) {
 
 //--------into routine in monitor--------------
      // LAB7 EXERCISE1: YOUR CODE
-     // I ate over
-     // test left and right neighbors
+      // I ate over 
+      state_condvar[i]=THINKING;
+      // test left and right neighbors
+      phi_test_condvar(LEFT);
+      phi_test_condvar(RIGHT);
 //--------leave routine in monitor--------------
      if(mtp->next_count>0)
         up(&(mtp->next));
